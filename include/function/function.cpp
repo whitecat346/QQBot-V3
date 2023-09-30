4f5eb5991712468758389dc9ecf2b244f73ac104
WhiteCAT <yanyuhang080901qw@outlook.com>
2023-10-01 00:41:25 +0800

    Add Score Code

---
+++ include/function/function.cpp
@@ -0,0 +1,17 @@
+#include "function.h"
+#include <string>
+
+void fileServer::cave(std::string& msg)
+{
+	
+}
+
+void qqBot::fecho(std::string& msg)
+{
+
+}
+
+void qqBot::fcave(std::string& msg)
+{
+	
+}
---
+++ include/function/function.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace fileServer
+{
+	void cave(std::string& msg);
+}
+
+namespace qqBot
+{
+	void fecho(std::string& msg);
+	void fcave(std::string& msg);
+}
