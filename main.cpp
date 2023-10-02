2ad550f314d82fdf69403fe53407ffc32dc50fc9
WhiteCAT <yanyuhang080901qw@outlook.com>
2023-10-02 11:22:17 +0800

    add server code

--- main.cpp
+++ main.cpp
@@ -3,10 +3,31 @@
 #include <iostream>
 #include <string>
 #include "include/hv/WebSocketServer.h"
+#include "include/hv/EventLoop.h"
+#include "include/hv/htime.h"
+#include <cstdlib>
 
-hv::WebSocketServer wsserver;
+hv::WebSocketService wsService;
 
-int main()
+int main(int argc, char ** argv)
 {
-	
+	wsService.onopen = [ ] (const WebSocketChannelPtr& channe, const HttpRequestPtr& req)
+		{
+			std::cout << "onopen: GET " << req->Path() << std::endl;
+		};
+	wsService.onmessage = [ ] (const WebSocketChannelPtr& channe, const std::string& msg)
+		{
+			std::cout << "onmessage: " << (int)msg.size() << msg.data() << std::endl;
+		};
+	wsService.onclose = [ ] (const WebSocketChannelPtr& channe)
+		{
+			std::cout << "onclose" << std::endl;
+		};
+
+	hv::WebSocketServer server(&wsService);
+	server.setPort(25565);
+	server.setThreadNum(4);
+	server.run();
+
+	return 0;
 }
